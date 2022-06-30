1ceb48e2d50cc84824c431a68546afe46730da05
Andrey Drozdov <drozdov_andrey@list.ru>
2022-06-30 21:35:58 +0300

    added tskexample1

---
+++ TaskExample1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите числа через пробел");
+            // Читаем строку из консоли
+            string str = Console.ReadLine();
+            // Вызов функции Split -- получение из строки массива по определённому разделителю
+            string[] nums = str.Split(' ');
+            // Перебор массива строк с помощью цикла
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i] + " ");
+            }
