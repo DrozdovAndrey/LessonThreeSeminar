93bc1daf58099f97c3cb62ce7dd14520b1728c1a
Andrey Drozdov <drozdov_andrey@list.ru>
2022-07-01 11:48:27 +0300

    added comments to Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -9,9 +9,31 @@ namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-           
+            // читаем число из консоли
+            int a = Convert.ToInt32(Console.ReadLine());
+            // получаем результат функции GetSquare
+            int[] squares = GetSquare(a);
+            // перебор массива с помощью цикла for
+            for (int i = 0; i < squares.Length; i++)
+            {
+                // вывод чисел в консоль
+                Console.WriteLine(squares[i] + " ");
+            }
+        }
+        //объявление функции
+        public static int[] GetSquare(int n)
+        {
+           //создание массива с размерностью n
+            int[] square = new int[n];
+            //объявление цикла от 1 до n включительно
+            for (int i = 1; i <= square.Length; i++)
+            {
+                // записываем в i - 1 элемент массива (по индексу) квадрата числа i
+                square[i-1] = i * i;    
+            }
+            return square;
         }
     }
 }
